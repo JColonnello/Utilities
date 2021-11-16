1bed38e43e01a6c0a73592a532a333dc6f715ba1
Joaquín Colonnello <joaquincolonnello@hotmail.com>
2021-11-16 14:51:56 -0300

    Changed signatures and added method
    Added Pool_GetRef
    Formatted code
    Changed handle argument type to typedef

--- src/collections/pool.c
+++ src/collections/pool.c
@@ -1,7 +1,7 @@
 #include <collections/pool.h>
-#include <stdlib.h>
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define DEFAULT_CAPACITY 32
@@ -18,37 +18,37 @@ struct Pool
 
 static void reallocMem(Pool *pool, int newSize)
 {
-	//Reserve memory for data and flags
+	// Reserve memory for data and flags
 	size_t dataSize = pool->elemSize * newSize;
 	size_t flagsSize = newSize / 8;
 	size_t oldFlagsSize = pool->size / 8;
 	uintptr_t *mem = realloc(pool->data, dataSize + flagsSize);
-	//Copy flags to new array (data is copied on realloc)
+	// Copy flags to new array (data is copied on realloc)
 	memcpy(mem + dataSize, pool->flags, oldFlagsSize);
-	//Initialize new flags to 0
+	// Initialize new flags to 0
 	size_t *ptr = mem + dataSize + oldFlagsSize;
-	for(size_t block = oldFlagsSize; block < flagsSize; block++, ptr++)
+	for (size_t block = oldFlagsSize; block < flagsSize; block++, ptr++)
 		*ptr = 0;
-	//Asign the new pointers and size
+	// Asign the new pointers and size
 	pool->data = mem;
-	pool->flags = (char*)(mem + dataSize);
+	pool->flags = (char *)(mem + dataSize);
 	pool->size = newSize;
 }
 
 Pool *Pool_Create(size_t elemSize)
 {
-	//Reserve memory
+	// Reserve memory
 	Pool *pool = malloc(sizeof(struct Pool));
-	if(pool == NULL)
+	if (pool == NULL)
 		return NULL;
-	
-	//Set variables
+
+	// Set variables
 	pool->size = 0;
 	pool->elemSize = elemSize;
 	pool->count = 0;
 	pool->maxCount = 0;
-	
-	//Reserve memory for data
+
+	// Reserve memory for data
 	pool->data = NULL;
 	pool->flags = NULL;
 	reallocMem(pool, DEFAULT_CAPACITY);
@@ -56,35 +56,36 @@ Pool *Pool_Create(size_t elemSize)
 	return pool;
 }
 
-int Pool_Add(Pool *pool, void *data)
+int Pool_Add(Pool *pool, const void *data)
 {
 	int pos = 0;
-	//If we have reached the end of the array, expland the Pool
-	if(pool->count == pool->size)
+	// If we have reached the end of the array, expand the Pool
+	if (pool->count == pool->size)
 		reallocMem(pool, pool->size * 2);
-	//If there are holes in the Pool check the flags
-	if(pool->count < pool->maxCount)
+	// If there are holes in the Pool check the flags
+	if (pool->count < pool->maxCount)
 	{
-		//Access the flags by byte and then shift to
-		//operate on the next bit
-		for(size_t block = 0; block < pool->size / 8; block++)
+		// Access the flags by byte and then shift to
+		// operate on the next bit
+		for (size_t block = 0; block < pool->size / 8; block++)
 		{
 			int chunk = pool->flags[block];
-			if(chunk == 255) continue;
-			for(int bit = 0; bit < 8; bit++, pos++)
+			if (chunk == 255)
+				continue;
+			for (int bit = 0; bit < 8; bit++, pos++)
 			{
-				//If a hole is found set that flag to 1 and leave
-				if(chunk <= 127) 
+				// If a hole is found set that flag to 1 and leave
+				if (chunk <= 127)
 				{
 					pool->flags[block] |= (uint8_t)128 >> bit;
 					goto out;
 				}
 			}
 		}
-		out:
+	out:
 		pool->count++;
 	}
-	//Otherwise Add the element at the end
+	// Otherwise Add the element at the end
 	else
 	{
 		int block = pool->count / 8;
@@ -93,7 +94,8 @@ int Pool_Add(Pool *pool, void *data)
 		pos = pool->count++;
 	}
 	memcpy(pool->data + pos * pool->elemSize, data, pool->elemSize);
-	if(pool->count > pool->maxCount) pool->maxCount = pool->count;
+	if (pool->count > pool->maxCount)
+		pool->maxCount = pool->count;
 	return pos;
 }
 
@@ -102,12 +104,19 @@ void Pool_Get(Pool *pool, int index, void *dest)
 	memcpy(dest, pool->data + pool->elemSize * index, pool->elemSize);
 }
 
+void *Pool_GetRef(Pool *pool, int index)
+{
+	if (index < 0 || (size_t)index >= pool->count)
+		return NULL;
+	return pool->data + pool->elemSize * index;
+}
+
 void Pool_Remove(Pool *pool, int index)
 {
 	int block = index / 8;
 	int offset = index % 8;
 
-	//Set flag to 0
+	// Set flag to 0
 	pool->flags[block] &= ~((uint8_t)128 >> offset);
 	pool->count--;
 }
@@ -121,14 +130,14 @@ int Pool_ToIndexArray(Pool *pool, int *array)
 {
 	int arrayPos = 0;
 	int poolPos = 0;
-	//Search the flags
-	for(size_t block = 0; block < pool->size / 8; block++)
+	// Search the flags
+	for (size_t block = 0; block < pool->size / 8; block++)
 	{
 		uint8_t chunk = pool->flags[block];
-		for(int bit = 0; bit < 8; bit++, poolPos++, chunk <<= 1)
+		for (int bit = 0; bit < 8; bit++, poolPos++, chunk <<= 1)
 		{
-			//If the flag is 1 copy index to array
-			if(chunk >= 128) 
+			// If the flag is 1 copy index to array
+			if (chunk >= 128)
 			{
 				array[arrayPos++] = poolPos;
 			}
@@ -141,32 +150,29 @@ int Pool_ToArray(Pool *pool, void *array)
 {
 	size_t arrayPos = 0;
 	size_t poolPos = 0;
-	//Search the flags
-	for(size_t block = 0; block < pool->size / 8; block++)
+	// Search the flags
+	for (size_t block = 0; block < pool->size / 8; block++)
 	{
 		uint8_t chunk = pool->flags[block];
 		/*If all the flags in the chunk are 1 copy the
-		* 8 elements to the array
-		*/
-		if(chunk == 255)
+		 * 8 elements to the array
+		 */
+		if (chunk == 255)
 		{
-			memcpy(array + pool->elemSize * arrayPos,
-			 		pool->data + pool->elemSize * poolPos,
-			 		 pool->elemSize * 8);
+			memcpy(array + pool->elemSize * arrayPos, pool->data + pool->elemSize * poolPos, pool->elemSize * 8);
 			arrayPos += 8;
 			poolPos += 8;
 		}
-		//Otherwise check the flags == 1 and copy their elements
-		else for(int bit = 0; bit < 8; bit++, poolPos++, chunk <<= 1)
-		{
-			if(chunk >= 128) 
+		// Otherwise check the flags == 1 and copy their elements
+		else
+			for (int bit = 0; bit < 8; bit++, poolPos++, chunk <<= 1)
 			{
-				memcpy(array + pool->elemSize * arrayPos,
-			 		pool->data + pool->elemSize * poolPos,
-			 		 pool->elemSize);
-				arrayPos++;
+				if (chunk >= 128)
+				{
+					memcpy(array + pool->elemSize * arrayPos, pool->data + pool->elemSize * poolPos, pool->elemSize);
+					arrayPos++;
+				}
 			}
-		}
 	}
 	return pool->count;
 }
--- src/collections/pool.h
+++ src/collections/pool.h
@@ -2,13 +2,15 @@
 #include <stddef.h>
 
 typedef struct Pool Pool;
+typedef int Handle;
 
 Pool *Pool_Create(size_t elemSize);
-int Pool_Add(Pool *pool, void *data);
-void Pool_Get(Pool *pool, int index, void *dest);
-void Pool_Remove(Pool *pool, int index);
+Handle Pool_Add(Pool *pool, const void *data);
+void Pool_Get(Pool *pool, Handle index, void *dest);
+void *Pool_GetRef(Pool *pool, Handle index);
+void Pool_Remove(Pool *pool, Handle index);
 int Pool_Count(Pool *pool);
-int Pool_ToIndexArray(Pool *pool, int *array);
+int Pool_ToIndexArray(Pool *pool, Handle *array);
 int Pool_ToArray(Pool *pool, void *array);
 void Pool_Dispose(Pool *pool);
 void *Pool_Data(Pool *pool);
